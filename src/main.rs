7c5b3da480b67bef1b866bf68c0cb195b63d35c5
Raphaël Duhen <raphael.duhen.nexus@gmail.com>
2021-10-30 05:57:05 +0200

    feat(test): Add basic tail logic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
+use bevy::math::Vec2;
+use bevy::prelude::*;
+use core::*;
+
+struct Tail {
+    joints: Vec<Vec2>,
+}
+
+struct Active;
+
+impl Tail {
+    pub fn new() -> Tail {
+        Tail { joints: Vec::new() }
+    }
+
+    pub fn init_joints(&mut self, amount: i32) {
+        for i in 0..amount {
+            self.joints.push(Vec2::new(i as f32, i as f32));
+        }
+    }
+}
+
+fn add_tails(mut commands: Commands) {
+    for _ in 0..10 {
+        commands.spawn().insert(Tail::new().init_joints(10));
+    }
+}
+
+fn tails_follow_cursor(mut query: Query<&mut Tail, With<Active>>) {
+    for mut tail in query.iter_mut() {
+        for joint_index in 1..tail.joints.len() {
+            tail.joints[joint_index] =
+                Vec2::lerp(tail.joints[joint_index],
+                           tail.joints[joint_index - 1],
+                           0.1);
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    App::build()
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(add_tails.system())
+        .add_system(tails_follow_cursor.system())
+        .run();
 }
